3e6a198549062e9deb2839344ca5d1551881616f
sreejith <squeeky.mouse@gmail.com>
2023-02-24 20:28:06 +0530

    add widgets, load csv, plot Sec32

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-# commit: initial commit Sec32
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+# commit: add widgets, load csv, plot Sec32
+
+df=pd.read_csv('happy.csv')
+
+st.title('In Search for Happiness')
+xaxis=st.selectbox(label='Select the data for x-axis',options=['GDP','Happiness','Generosity'])
+yaxis=st.selectbox(label='Select the data for y-axis',options=['GDP','Happiness','Generosity'])
+
+st.subheader(f'{xaxis} and {yaxis}')
+
+def get_data(x:str,y:str):
+    ax=list(df[x])
+    ay=list(df[y])
+    return(ax,ay)
+
+x,y=get_data(xaxis.lower(),yaxis.lower())
+
+# figure=px.line(x=x,y=y,labels={'x':xaxis,'y':yaxis})
+figure=px.scatter(x=x,y=y,labels={'x':xaxis,'y':yaxis})
+st.plotly_chart(figure)
